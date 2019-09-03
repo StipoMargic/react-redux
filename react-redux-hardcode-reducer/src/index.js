5e3261121555e98e5d72c3a279564f736b1d9f2f
StipoMar <stipo.margic@gmail.com>
2019-09-03 17:51:56 +0200

    Redux-dev-tools

--- react-redux-hardcode-reducer/src/index.js
+++ react-redux-hardcode-reducer/src/index.js
@@ -5,12 +5,13 @@ import './css/style.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
 
 const store = createStore(
 	reducers,
-	applyMiddleware(thunk),
+	composeWithDevTools(applyMiddleware(thunk)),
 );
 
 ReactDOM.render(
